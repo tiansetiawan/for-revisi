7988c7f09adcb78842157c2ce212d76e9203d8ad
unknown <tianahmad13@hotmail.com>
2025-07-25 16:51:25 +0700

    update tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from '@/app/components/navbar';
 import Footer from '@/app/components/Footer';
 import SessionStorageSync from "./components/SessionStorageSync";
+import Script from "next/script";
 
 
 const geistSans = Geist({
@@ -33,22 +34,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <head>
-          <script
+<head>
+        {/* Google Tag Manager - recommended by Next.js */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MWBFCWPF');
+          `}
+        </Script>
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript fallback) */}
+        <noscript
           dangerouslySetInnerHTML={{
             __html: `
-              (function() {
-                var ahrefs_analytics_script = document.createElement('script');
-                ahrefs_analytics_script.async = true;
-                ahrefs_analytics_script.src = 'https://analytics.ahrefs.com/analytics.js';
-                ahrefs_analytics_script.setAttribute('data-key', 'qtLJTglbhK9QiMuck0C+uw');
-                document.getElementsByTagName('head')[0].appendChild(ahrefs_analytics_script);
-              })();
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MWBFCWPF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
             `,
           }}
         />
-      </head>
-      <body>
           <SessionStorageSync/>
           <Navbar />
           {children}
