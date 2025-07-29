39c4ca20fdc7e76803b9dfbb57ecee7abf7b02e9
unknown <tianahmad13@hotmail.com>
2025-07-29 16:23:20 +0700

    update data responsive

--- src/app/beranda/FadeBannerSlider.js
+++ src/app/beranda/FadeBannerSlider.js
@@ -18,7 +18,7 @@ const FadeBannerSlider = ({
   const sliderRef = useRef(null);
   const [isHovered, setIsHovered] = useState(false);
   const [isPlaying, setIsPlaying] = useState(true);
-  const isMobile = useMediaQuery({ query: '(max-width: 378px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 440px)' });
 
   useEffect(() => {
     if (sliderRef.current) {
