79f8c7e2e7fb92d2d955e6ede843a196bedb504c
unknown <tianahmad13@hotmail.com>
2025-08-04 13:56:44 +0700

    update beranda mb4

---
+++ src/app/components/useIsMobile.js
@@ -0,0 +1,21 @@
+'use client';
+import { useState, useEffect } from 'react';
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 430);
+    };
+    
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    };
+  }, []);
+
+  return isMobile;
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -35,17 +35,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
 <head>
-        {/* Google Tag Manager - recommended by Next.js */}
-        <Script id="gtm-init" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MWBFCWPF');
-          `}
-        </Script>
-      </head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  {/* Google Tag Manager - recommended by Next.js */}
+  <Script id="gtm-init" strategy="afterInteractive">
+    {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MWBFCWPF');
+    `}
+  </Script>
+</head>
       <body>
         {/* Google Tag Manager (noscript fallback) */}
         <noscript
