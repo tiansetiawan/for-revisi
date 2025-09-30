fd4dae51fe9eb1b48c4705e8ae99bfa4317baa60
tiansetiawan <tiansetiawan.1331@gmail.com>
2025-09-30 14:04:36 +0700

    update artikel visit

--- src/app/blog/artikel/detail-e/Detail-e.jsx
+++ src/app/blog/artikel/detail-e/Detail-e.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from "next/link";
 import { useState, useEffect } from 'react';
 import { FaPlay, FaTimes } from 'react-icons/fa';
+import '@/app/style/Blog.css';
 
 
 // Video Data
